29ef9bc17ced2c659e47828cf8d44bb31ecd7d84
Freedman <victor2oxide@gmail.com>
2023-07-13 14:55:12 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("size of a char: %lu byte(s)\n", sizeof(a));
 	printf("size of an int: %lu byte(s)\n", sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", sizeof(c));
